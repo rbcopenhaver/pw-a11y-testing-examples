1e831e39c5687a05067947fa70469e751907e177
rbcopenhaver <richard.copenhaver@gmail.com>
2025-09-29 19:49:06 -0400

    Added axe-core dependency,  mobile devices, and accessibility test for webpage.

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,14 +52,15 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+     {
+       name: 'Mobile Chrome',
+       use: { ...devices['Pixel 10'] },
+     },
+     
+     {
+       name: 'Mobile Safari',
+       use: { ...devices['iPhone 16'] },
+     },
 
     /* Test against branded browsers. */
     // {
